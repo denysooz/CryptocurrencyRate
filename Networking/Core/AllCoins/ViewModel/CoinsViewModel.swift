7a04e09fb1dfdf25baad6d87ef14d4c8bb9aa08a
denysooz <den.darevskiy@mail.ru>
2024-07-28 19:56:53 -0400

    Parsing incoming JSON data

--- Networking/Core/AllCoins/ViewModel/CoinsViewModel.swift
+++ Networking/Core/AllCoins/ViewModel/CoinsViewModel.swift
@@ -13,16 +13,28 @@ class CoinsViewModel: ObservableObject {
     @Published var price = ""
     
     init() {
-        fetchPrice()
+        fetchPrice(coin: "litecoin")
     }
     
-    func fetchPrice() {
-        let urlString = "https://api.coingecko.com/api/v3/simple/price?ids=near&vs_currencies=usd"
+    func fetchPrice(coin: String) {
+        let urlString = "https://api.coingecko.com/api/v3/simple/price?ids=\(coin)&vs_currencies=usd"
         guard let url = URL(string: urlString) else { return }
         
         print("Fetcing price...")
         URLSession.shared.dataTask(with: url) { data, response, error in
+            guard let data = data else { return }
+            
             print("Did receive data \(data)")
+            
+            guard let jsonObjest = try? JSONSerialization.jsonObject(with: data) as? [String: Any] else { return }
+            
+            guard let value = jsonObjest[coin] as? [String: Any] else { return }
+            guard let price = value["usd"] else { return }
+            
+            DispatchQueue.main.async {
+                self.coin = coin.capitalized
+                self.price = "$\(price)"
+            }
         }.resume()
         
         print("Did reach end of function")
