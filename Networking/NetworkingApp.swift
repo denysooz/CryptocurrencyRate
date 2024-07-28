5058bc29cd3da84e2b2bbddb18d852bc0b0a08aa
denysooz <den.darevskiy@mail.ru>
2024-07-27 22:31:09 -0400

    Initial Commit

---
+++ Networking/NetworkingApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkingApp.swift
+//  Networking
+//
+//  Created by Denis Dareuskiy on 27.07.24.
+//
+
+import SwiftUI
+
+@main
+struct NetworkingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
