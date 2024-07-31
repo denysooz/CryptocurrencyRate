0c097a1a49df9dd998671841376be5f5ae1a2f87
denysooz <den.darevskiy@mail.ru>
2024-07-30 23:19:05 -0500

    Added error handling

--- Networking/Core/AllCoins/Service/CoinDataService.swift
+++ Networking/Core/AllCoins/Service/CoinDataService.swift
@@ -11,14 +11,18 @@ class CoinDataService {
     
     private let urlString = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=40&page=1&sparkline=false&price_change_percentage=24h&locale=en"
     
-    func fetchCoins(completion: @escaping([Coin]) -> Void) {
+    func fetchCoins(completion: @escaping(Result<[Coin], Error>) -> Void) {
         guard let url = URL(string: urlString) else { return }
         
         URLSession.shared.dataTask(with: url) { data, response, error in
+            if let error = error {
+                completion(.failure(error))
+                return
+            }
             guard let data = data else { return }
             
             guard let coins = try? JSONDecoder().decode([Coin].self, from: data ) else { return }
-            completion(coins)
+            completion(.success(coins))
         }.resume()
     }
     
--- Networking/Core/AllCoins/ViewModel/CoinsViewModel.swift
+++ Networking/Core/AllCoins/ViewModel/CoinsViewModel.swift
@@ -10,6 +10,7 @@ import Foundation
 class CoinsViewModel: ObservableObject {
     
     @Published var coins = [Coin]()
+    @Published var errorMessage: String?
     
     private let service = CoinDataService()
     
@@ -18,9 +19,23 @@ class CoinsViewModel: ObservableObject {
     }
     
     func fetchCoins() {
-        service.fetchCoins { coins in
+//        service.fetchCoins { coins, error in
+//            DispatchQueue.main.async {
+//                if let error = error {
+//                    self.errorMessage = error.localizedDescription
+//                    return
+//                }
+//                self.coins = coins ?? []
+//            }
+//        }
+        service.fetchCoins { result in
             DispatchQueue.main.async {
-                self.coins = coins
+                switch result {
+                case .success(let coins):
+                    self.coins = coins
+                case .failure(let error):
+                    self.errorMessage = error.localizedDescription
+                }
             }
         }
     }
